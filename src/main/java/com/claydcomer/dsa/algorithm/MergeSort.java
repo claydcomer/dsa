bba4c90094303f63d67a51991dc0253cc98df7be
Clay Comer <7101603+claydcomer@users.noreply.github.com>
2023-07-20 10:54:21 -0400

    Adding implementation of MergeSort.

---
+++ src/main/java/com/claydcomer/dsa/algorithm/MergeSort.java
@@ -0,0 +1,37 @@
+package com.claydcomer.dsa.algorithm;
+
+public class MergeSort extends IntegerArraySortAlgorithm {
+    @Override
+    public void sort() {
+        mergeSort(integersToSort, 0, integersToSort.length);
+    }
+
+    private void mergeSort(int[] input, int start, int end) {
+        if(end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    private void merge(int[] input, int start, int mid, int end) {
+        if(input[mid - 1] <= input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while(i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+    }
+}
