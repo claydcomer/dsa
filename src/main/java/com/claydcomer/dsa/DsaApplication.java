00e5de332bea4cc5379f1d73ff6bd4e25e689924
claydcomer <7101603+claydcomer@users.noreply.github.com>
2023-09-12 10:50:46 -0400

    Stacks (#5)
    * Add stack with array backing implementation
    
    * Add stack with linked list backing implementation

--- src/main/java/com/claydcomer/dsa/DsaApplication.java
+++ src/main/java/com/claydcomer/dsa/DsaApplication.java
@@ -1,5 +1,8 @@
 package com.claydcomer.dsa;
 
+import com.claydcomer.dsa.datastructure.stack.ArrayStack;
+import com.claydcomer.dsa.datastructure.stack.LinkedStack;
+import com.claydcomer.dsa.model.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,12 @@ public class DsaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsaApplication.class, args);
+
+		Employee e1 = new Employee("Jane", "Jones", 123);
+		Employee e2 = new Employee("John", "Doe", 4567);
+		Employee e3 = new Employee("Mary", "Smith", 22);
+		Employee e4 = new Employee("Mike", "Wilson", 3245);
+		Employee e5 = new Employee("Bill", "End", 78);
 	}
 
 }
---
+++ src/main/java/com/claydcomer/dsa/datastructure/stack/Stack.java
@@ -0,0 +1,10 @@
+package com.claydcomer.dsa.datastructure.stack;
+
+public interface Stack<T> {
+
+    void push(T data);
+
+    T pop();
+
+    T peek();
+}
